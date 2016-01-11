e22da2fee87135fa7317d4145a9068e0fbc4a9c1
Borislav Sapundzhiev <bsapundjiev@gmail.com>
2016-01-11 14:18:01 +0200

    GH361 - encode file path

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@ define(function(require, exports, module) {
     console.log("Initalization Browser PDF Viewer...");
     $('#' + elementID).append($('<iframe>', {
       id: "iframeViewer",
-      src: "libs/pdfjs/web/viewer.html?file=" + filePath,
+      src: "libs/pdfjs/web/viewer.html?file=" + encodeURIComponent(filePath),
       "nwdisable": "",
       "nwfaketop": ""
     }));
