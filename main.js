5448e02831dd92aeabeb4d2d4bf1f3467fddff1e
kristiyandd <kristiyandd@gmail.com>
2018-01-10 13:38:18 +0200

    cleaned content

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 /* Copyright (c) 2013-present The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-"use strict";
+'use strict';
 
 (function() {
   function getParameterByName (name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
   }
 
-  var locale = getParameterByName("locale");
-  var filePath = getParameterByName("file") || '../../example.pdf';
+  var locale = getParameterByName('locale');
+  var filePath = getParameterByName('file') || '../../example.pdf';
 
   document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
