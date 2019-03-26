fb892b4cc1acace8853f5b5d7b30b7a7a022eba8
Ilian Sapundshiev <ilian@tagspaces.org>
2019-03-26 22:35:52 +0100

    using the extcommons lib

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 /* Copyright (c) 2013-present The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-'use strict';
-
 (() => {
-  function getParameterByName (paramName) {
-    const name = paramName.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
-    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    const results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  }
-
   // const locale = getParameterByName('locale');
   const filePath = getParameterByName('file') || '../../example.pdf';
 
